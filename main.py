76064eb986dd431f0430a388c25a972cb60ead14
ThDag <tahadoktor@gmail.com>
2023-02-10 16:20:13 +0300

    made img_name usiversal, added the ability to save image in show_image

--- main.py
+++ main.py
@@ -4,27 +4,22 @@ from PIL import Image
 import urllib.request
 import json
 
-Criosity_cameras = {0: 'fhaz',
-                    1: 'rhaz',
-                    2: 'mast',
-                    3: 'chemcam',
-                    4: 'mahli',
-                    5: 'mardi',
-                    6: 'navcam'}
+Criosity_cameras = {0: 'fhaz', 1: 'rhaz', 2: 'mast', 3: 'chemcam', 4: 'mahli', 5: 'mardi', 6: 'navcam'}
 
-class main():
-    def __init__(self, api_key: str, save: bool = False, img_dir: str = '.'):
+class Main():
+    def __init__(self, api_key: str, save: bool = False, img_dir: str = '.', img_name: str = 'potd'):
         self.api_key = api_key
         self.img_dir = img_dir + '/'
         self.save = save
+        self.img_name = img_name
 
-    def apod(self, img_name: str = 'potd'): # TODO integrate main class
+    def apod(self): # TODO make its own class as said
         response = requests.request('GET', f'https://api.nasa.gov/planetary/apod?api_key={self.api_key}')
-        urllib.request.urlretrieve(response.json()['url'], f'{self.img_dir}./{img_name}.jpg')
+        urllib.request.urlretrieve(response.json()['url'], f'{self.img_dir}./{self.img_name}.jpg')
         Image.open('potd.jpg')
 
 
-class Criosity:
+class Criosity(Main):
     def __init__(self, camera: int, date: str, page: int):
         self.camera = Criosity_cameras.get(camera)
         self.date = date
@@ -40,19 +35,26 @@ class Criosity:
     def show_image(self, image_idx: int): # add image download function with img_dir value ingtagration
         # print(json.dumps(parametres, indent=2))
         # response = requests.request('GET', url, params=parametres) 
-        # print(response)
+        
+        if self.save:
+            response = self.response.json()['photos'][image_idx]
+            urllib.request.urlretrieve(response.json()['url'], f'{self.img_dir}./{self.img_name}.jpg')
+            # Image.open(f'{self.img_dir}./{self.img_name}.jpg')
+                    
+        else:
+            image_data = self.response.json()['photos'][image_idx]
+            # print(json.dumps(image_data, indent=2))
 
-        image_data = self.response.json()['photos'][image_idx]
-        # print(json.dumps(image_data, indent=2))
-
-        image_url = image_data['img_src']
-        # TODO: show the image
-        return image_url     
+            image_url = image_data['img_src']
+            # TODO: show the image
+            return image_url     
 
 rover = Criosity(camera=2, date='2022-03-23', page=1)
 
 # print('humber of images in the page: ',  rover.image_number())
 # print(rover.show_image(0))
 
-apoding = main('DEMO_KEY')
+# TODO: make a apod class containing 1. given day of apod; if not givin today, 2. think what else about apod
+
+apoding = Main('DEMO_KEY')
 apoding.apod()
